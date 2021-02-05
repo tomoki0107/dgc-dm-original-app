49b7fe7bce56d25cb0c684bae097686beca7bc82
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-05 18:47:07 +0900

    ルーティング、コントローラー、ビューの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'cards/index'
   devise_for :users
   root to: "articles#index"
   resources :articles, only: [:index]
+  resources :cards, only: [:index]
 end
