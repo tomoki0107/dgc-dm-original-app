96c96590a910c519bb9e6b54f781b5cef8fa4e18
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-06 22:42:23 +0900

    カラム名の変更

--- db/migrate/20210206130313_create_pack_rarity_rates.rb
+++ db/migrate/20210206130313_create_pack_rarity_rates.rb
@@ -2,7 +2,7 @@ class CreatePackRarityRates < ActiveRecord::Migration[6.0]
   def change
     create_table :pack_rarity_rates do |t|
       t.string     :rarity
-      t.integer    :weight
+      t.integer    :rarity_weight
       t.references :pack, foreign_key: true
       t.timestamps
     end
--- db/migrate/20210206130327_create_pack_card_rates.rb
+++ db/migrate/20210206130327_create_pack_card_rates.rb
@@ -1,8 +1,8 @@
 class CreatePackCardRates < ActiveRecord::Migration[6.0]
   def change
     create_table :pack_card_rates do |t|
-      t.string :card_name
-      t.integer :weight
+      t.string     :card_name
+      t.integer    :card_weight
       t.references :pack_rarity_rate, foreign_key: true
       t.timestamps
     end
