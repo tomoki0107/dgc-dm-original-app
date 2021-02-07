c687904383fb8a556ba7100c7c491b44578ed128
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-07 17:49:11 +0900

    抽選メソッドの作成

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -4,4 +4,22 @@ class PacksController < ApplicationController
     @packs = Pack.all
   end
 
+  def show
+    @pack = Pack.find(params[:id])
+    if @pack.id == 1
+      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5) #第1弾パックのレアリティテーブルに該当するid1~5を取得
+    elsif @pack.id == 2
+      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(5) #第2弾パックのレアリティテーブルに該当するid6~10を取得
+    elsif @pack.id == 3
+      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(10) #第3弾パックのレアリティテーブルに該当するid11~15を取得
+    elsif @pack.id == 4
+      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(15) #第4弾パックのレアリティテーブルに該当するid16=20を取得
+    elsif @pack.id == 5
+      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(20) #第5弾パックのレアリティテーブルに該当するid21~25を取得
+    elsif @pack.id == 6
+      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(25) #第6弾パックのレアリティテーブルに該当するid26~30を取得
+    else
+      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(30) #第7弾パックのレアリティテーブルに該当するid31~35を取得
+  end
+
 end
--- app/models/pack.rb
+++ app/models/pack.rb
@@ -1,3 +1,13 @@
 class Pack < ApplicationRecord
   has_many :pack_rarity_rates
+
+  def rarity_lottery1
+    rarity = rand(200)
+    if rarity < @pack_rarity_rate.limit(1).weight #rarityの数値が取得したレアリティテーブルの先頭のweightカラムの値より大きければ
+    elsif rarity < @pack_rarity_rate.limit(1).offset(1).weight #rarityの数値が取得したレアリティテーブルの2番目のweightカラムの値より大きければ
+    elsif rarity < @pack_rarity_rate.limit(1).offset(2).weight #rarityの数値が取得したレアリティテーブルの3番目のweightカラムの値より大きければ
+    elsif rarity < @pack_rarity_rate.limit(1).offset(3).weight #rarityの数値が取得したレアリティテーブルの4番目のweightカラムの値より大きければ
+    else rarity < @pack_rarity_rate.limit(1).offset(4).weight #rarityの数値が取得したレアリティテーブルの5番目のweightカラムの値より大きければ
+  end 
+
 end
