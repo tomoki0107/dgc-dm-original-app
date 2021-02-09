53768111df48938bd82e25356c4ff917a80123ab
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-09 18:11:19 +0900

    binding.pryの導入とpacks#showの実装

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -6,20 +6,17 @@ class PacksController < ApplicationController
 
   def show
     @pack = Pack.find(params[:id])
-    if @pack.id == 1
-      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5) #第1弾パックのレアリティテーブルに該当するid1~5を取得
-    elsif @pack.id == 2
-      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(5) #第2弾パックのレアリティテーブルに該当するid6~10を取得
-    elsif @pack.id == 3
-      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(10) #第3弾パックのレアリティテーブルに該当するid11~15を取得
-    elsif @pack.id == 4
-      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(15) #第4弾パックのレアリティテーブルに該当するid16=20を取得
-    elsif @pack.id == 5
-      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(20) #第5弾パックのレアリティテーブルに該当するid21~25を取得
-    elsif @pack.id == 6
-      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(25) #第6弾パックのレアリティテーブルに該当するid26~30を取得
-    else
-      @pack_rarity_rate = @pack.pack_rarity_rate.limit(5)offset(30) #第7弾パックのレアリティテーブルに該当するid31~35を取得
+    @pack_rarity_rates = @pack.pack_rarity_rates
+    @rarity_lottery = Pack.rarity_lottery
   end
 
+  private
+
+  def pack_params
+    params.require(:pack).permit(:pack_name, :pack_image)
+  end
+
+  def pack_rarity_rate_params
+    params.require(:pack_rarity_rate).permit(:rarity, :rarity_weight).merge(pack_id: params[:pack_id])
+  end
 end
--- app/models/pack.rb
+++ app/models/pack.rb
@@ -1,13 +1,19 @@
 class Pack < ApplicationRecord
   has_many :pack_rarity_rates
 
-  def rarity_lottery1
+  def self.rarity_lottery
     rarity = rand(200)
-    if rarity < @pack_rarity_rate.limit(1).weight #rarityの数値が取得したレアリティテーブルの先頭のweightカラムの値より大きければ
-    elsif rarity < @pack_rarity_rate.limit(1).offset(1).weight #rarityの数値が取得したレアリティテーブルの2番目のweightカラムの値より大きければ
-    elsif rarity < @pack_rarity_rate.limit(1).offset(2).weight #rarityの数値が取得したレアリティテーブルの3番目のweightカラムの値より大きければ
-    elsif rarity < @pack_rarity_rate.limit(1).offset(3).weight #rarityの数値が取得したレアリティテーブルの4番目のweightカラムの値より大きければ
-    else rarity < @pack_rarity_rate.limit(1).offset(4).weight #rarityの数値が取得したレアリティテーブルの5番目のweightカラムの値より大きければ
+    if rarity < 3 #rarityの数値が取得したレアリティテーブルの先頭のweightカラムの値より小さければ
+      return @pack_rarity_rates.limit(1)
+    elsif rarity < 12 #rarityの数値が取得したレアリティテーブルの2番目のweightカラムの値より小さければ
+      return @pack_rarity_rates.limit(1).offset(1)
+    elsif rarity < 36 #rarityの数値が取得したレアリティテーブルの3番目のweightカラムの値より小さければ
+      return @pack_rarity_rates.limit(1).offset(2)
+    elsif rarity < 96 #rarityの数値が取得したレアリティテーブルの4番目のweightカラムの値より小さければ
+      return @pack_rarity_rates.limit(1).offset(3)
+    else  #rarityの数値が取得したレアリティテーブルの5番目のweightカラムの値より小さければ
+      return @pack_rarity_rates.limit(1).offset(4)
+    end
   end 
 
 end
