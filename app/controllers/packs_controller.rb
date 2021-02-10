e9062e686e4e9a4bb3c1b49787b772152eaec1e4
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-10 23:26:38 +0900

    ガチャ実装(1枚のカード情報の表示まで)

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -8,22 +8,7 @@ class PacksController < ApplicationController
     pack = Pack.find(params[:id])
     pack_rarity_rates = pack.pack_rarity_rates
     rarity_lottery = Pack.rarity_lottery(pack_rarity_rates)
-    pack_card_rates = rarity_lottery.pack_card_rates
-    @card_lottery = Pack.card_lottery(pack_card_rates)
-  end
-
-  private
-
-  def pack_params
-    params.require(:pack).permit(:pack_name, :pack_image)
-  end
-
-  def pack_rarity_rate_params
-    params.require(:pack_rarity_rate).permit(:rarity, :rarity_weight).merge(pack_id: params[:pack_id])
-  end
-
-  def pack_card_rate_params
-    params.require(:pack_card_rate).permit(:card_name, :card_weight).merge(pack_rarity_rate_id: params[:pack_rarity_rate_id])
+    @card_lottery = Pack.card_lottery(rarity_lottery.pack_card_rates)
   end
 
 end
