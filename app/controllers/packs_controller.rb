6be2dbe38d5f1f534be0890625fd17dc8de58271
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-14 01:11:30 +0900

    1パックのカードの結果表示の実装

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -7,8 +7,12 @@ class PacksController < ApplicationController
   def show
     pack = Pack.find(params[:id])
     pack_rarity_rates = pack.pack_rarity_rates
-    rarity_lottery = Pack.rarity_lottery(pack_rarity_rates)
-    @card_lottery = Pack.card_lottery(rarity_lottery.pack_card_rates)
+    @card = []
+    5.times do
+      rarity_lottery = Pack.rarity_lottery(pack_rarity_rates)
+      card_lottery = Pack.card_lottery(rarity_lottery.pack_card_rates)
+      @card << card_lottery
+    end
   end
 
 end
