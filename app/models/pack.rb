7696c69f28c35ae8cca82f62526e10690b32e74a
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-13 18:11:13 +0900

    重み付け抽選メソッドの作成(修正1)

--- app/models/pack.rb
+++ app/models/pack.rb
@@ -2,19 +2,20 @@ class Pack < ApplicationRecord
   has_many :pack_rarity_rates
 
   def self.rarity_lottery(pack_rarity_rates)
-    weight = [3, 9, 24, 60, 104]
-    total_weight = weight.inject(:+)
+    ids = pack_rarity_rates.pluck(:id)
+    weights = pack_rarity_rates.pluck(:rarity_weight)
+    total_weight = weights.inject(:+)
     pick = nil
     rnd = rand(total_weight)
   
-    weight.each_with_index do |item, i|
-      if rnd < weight[i]
-        pick = item
+    weights.each_with_index do |item, i|
+      if rnd < weights[i]
+        pick = i
         break
       end
-      rnd -= weight[i]
+      rnd -= weights[i]
     end
-    return pack_rarity_rates.where(rarity_weight: pick)
+    return pack_rarity_rates.find(ids[pick])
   end
   
   def self.card_lottery(pack_card_rates)
