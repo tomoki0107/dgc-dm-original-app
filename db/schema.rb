13d4a4144347bbe9b2c544dfd97c1208d8ccd1e3
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-15 19:21:04 +0900

    記事一覧ページの作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,13 @@
 ActiveRecord::Schema.define(version: 2021_02_06_130327) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "deck_url"
     t.text "content"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
