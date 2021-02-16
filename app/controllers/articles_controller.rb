ebdb582279bbd38e68eb5dbb73bd9f32bb236be2
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-16 21:04:14 +0900

    edit,updateアクションの実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,17 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
   end
   
   private
