96ac0dc7a497043243ad8fd66b37a755a125a8b6
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-16 18:58:02 +0900

    new,editアクションの追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,14 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
   end
   
 end
