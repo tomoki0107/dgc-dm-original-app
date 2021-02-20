f2646f2fe38b807a9e75cefff0fc2ebd85bb1a15
tomoki0107 <tttttnozawa0340@gmail.com>
2021-02-20 23:31:35 +0900

    カード詳細画面のコメント欄の実装

---
+++ app/controllers/card_comments_controller.rb
@@ -0,0 +1,13 @@
+class CardCommentsController < ApplicationController
+  def create
+    card_comment = CardComment.create(card_comment_params)
+    redirect_to "/cards/#{card_comment.card.id}"
+  end
+
+  private
+
+  def card_comment_params
+    params.require(:card_comment).permit(:text).merge(user_id: current_user.id, card_id: params[:card_id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
   resources :articles
-  resources :cards, only: [:index, :show]
+  resources :cards, only: [:index, :show] do
+    resources :card_comments, only: :create
+  end
   resources :packs, only: [:index, :show]
 end
