3bcc899331db4b751521b2b74daa7f2ad12ef5af
tomoki0107 <tttttnozawa0340@gmail.com>
2021-03-02 01:38:33 +0900

    bulletの導入とN+1問題の解決

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :search]
 
   def index
-    @articles = Article.all
+    @articles = Article.includes(:user)
   end
 
   def new
