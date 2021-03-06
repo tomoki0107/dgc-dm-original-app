65bb77c2594e484e691130daa3d8b1aee4d59fd9
tomoki0107 <tttttnozawa0340@gmail.com>
2021-03-07 05:12:18 +0900

    記事詳細画面のデザイン編集

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
     @article_comment = ArticleComment.new
-    @article_comments  = @article.article_comments.includes(:user)
+    @article_comments  = @article.article_comments.includes(:user).order("created_at DESC")
   end
 
   def edit
