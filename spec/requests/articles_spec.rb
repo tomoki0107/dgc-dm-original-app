b6ea498c81366f99475f2bd29ae3bfaba72b2549
tomoki0107 <tttttnozawa0340@gmail.com>
2021-03-13 21:18:52 +0900

    共通の処理のまとめ

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -6,43 +6,44 @@ describe ArticlesController, type: :request do
   end
 
   describe 'GET #index' do
-    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
+
+    before do
       get root_path
+    end
+
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
       expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みの記事のタイトルが存在する' do 
-      get root_path
       expect(response.body).to include(@article.title)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みの記事のユーザー名が存在する' do
-      get root_path
       expect(response.body).to include(@article.user.username) 
     end
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
-      get root_path
       expect(response.body).to include('投稿を検索する')
     end
   end
 
   describe 'GET #show' do
-    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+
+    before do
       get article_path(@article)
+    end
+
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
       expect(response.status).to eq 200
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みの記事のタイトルが存在する' do 
-      get article_path(@article)
       expect(response.body).to include(@article.title) 
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みの記事のデッキURLが存在する' do 
-      get article_path(@article)
       expect(response.body).to include(@article.deck_url) 
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みの記事のユーザー名が存在する' do
-      get article_path(@article)
       expect(response.body).to include(@article.user.username)  
     end
     it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
-      get article_path(@article)
       expect(response.body).to include('＜コメント一覧＞')
     end
   end 
--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -6,78 +6,70 @@ describe CardsController, type: :request do
   end
 
   describe 'GET #index' do
-    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
+
+    before do
       get cards_path
+    end
+
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
       expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスにカード名が存在する' do 
-      get cards_path
       expect(response.body).to include(@card.name)
     end
     it 'indexアクションにリクエストするとレスポンスにカードの文明が存在する' do 
-      get cards_path
       expect(response.body).to include(@card.civilization)
     end
     it 'indexアクションにリクエストするとレスポンスにカードのコストが存在する' do 
-      get cards_path
       expect(response.body).to include(@card.cost)
     end
     it 'indexアクションにリクエストするとレスポンスにカードのレア度が存在する' do 
-      get cards_path
       expect(response.body).to include(@card.rarity)
     end
     it 'indexアクションにリクエストするとレスポンスにカードの画像(URL)が存在する' do 
-      get cards_path
       expect(response.body).to include(@card.card_image)
     end
     it 'indexアクションにリクエストするとレスポンスにカード検索フォームが存在する' do 
-      get cards_path
       expect(response.body).to include('カードを検索する')
     end
 
     describe 'GET #show' do
-      it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+
+      before do
         get card_path(@card)
+      end
+
+      it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
         expect(response.status).to eq 200
       end
       it 'showアクションにリクエストするとレスポンスにカード名が存在する' do 
-        get card_path(@card)
         expect(response.body).to include(@card.name)
       end
       it 'showアクションにリクエストするとレスポンスにカードの種族が存在する' do 
-        get card_path(@card)
         expect(response.body).to include(@card.race)
       end
       it 'showアクションにリクエストするとレスポンスにカードのタイプが存在する' do 
-        get card_path(@card)
         expect(response.body).to include(@card.category)
       end
       it 'showアクションにリクエストするとレスポンスにカードの文明が存在する' do 
-        get card_path(@card)
         expect(response.body).to include(@card.civilization)
       end
       it 'showアクションにリクエストするとレスポンスにカードのレア度が存在する' do 
-        get card_path(@card)
         expect(response.body).to include(@card.rarity)
       end
       it 'showアクションにリクエストするとレスポンスにカードのコストが存在する' do 
-        get card_path(@card)
         expect(response.body).to include(@card.cost)
       end
       it 'showアクションにリクエストするとレスポンスにカードのパワーが存在する' do 
-        get card_path(@card)
         expect(response.body).to include(@card.power)
       end
       it 'showアクションにリクエストするとレスポンスにカードの効果が存在する' do 
-        get card_path(@card)
         expect(response.body).to include(@card.effect)
       end
       it 'showアクションにリクエストするとレスポンスにカードの画像(URL)が存在する' do 
-        get card_path(@card)
         expect(response.body).to include(@card.card_image)
       end
       it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
-        get card_path(@card)
         expect(response.body).to include('＜コメント一覧＞')
       end
     end
