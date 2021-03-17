b89a80861421386d517a61d3678794354a5a3001
tomoki0107 <tttttnozawa0340@gmail.com>
2021-03-18 00:02:56 +0900

    抽選メソッドの修正4

--- spec/factories/pack_rarity_rates.rb
+++ spec/factories/pack_rarity_rates.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :pack_rarity_rate do
     rarity   {Faker::Team.creature}
     weight  {Faker::Number.between(from: 1, to: 10)}
-    association :pack
+    association :pack, factory: :pack
   end
 end
--- spec/models/pack_spec.rb
+++ spec/models/pack_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Pack, type: :model do
 
-  let(:records) { FactoryBot.create_list(:pack_rarity_rate, 3) }
+  let(:pack) { FactoryBot.create(:pack_with_pack_rarity_rate) }
   describe '抽選メソッド' do
     it '抽選メソッドが機能していること' do
-      result = Pack.lottery(records)
-      binding.pry
+      result = Pack.lottery(pack.pack_rarity_rates)
       expect(result).to be_instance_of(PackRarityRate)
     end
   end
 end
+
+
+
 
